947c8b1e7b10487c86eb86ab51c3ae075720aa68
Jonathan <j.milne21@gmail.com>
2019-09-06 18:57:15 +0000

    tree practice

--- vampire.js
+++ vampire.js
@@ -10,22 +10,29 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    let numberOfVampires = 0;
+    let vampire = this;
+    while (vampire.creator) {
+      vampire = vampire.creator;
+      numberOfVampires ++;
+    }
+    return numberOfVampires;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    return this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal;
   }
 
   /** Stretch **/
@@ -37,6 +44,23 @@ class Vampire {
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
 
+    let myAncestors = [this];
+    let vampireAncestors = [vampire];
+    let myAncestor = this;
+    let vampAncestor = vampire;
+
+    while (myAncestor.creator) {
+      myAncestors.push(myAncestor.creator);
+      myAncestor = myAncestor.creator;
+    }
+    while (vampAncestor.creator) {
+      vampireAncestors.push(vampAncestor.creator);
+      vampAncestor = vampAncestor.creator;
+    }
+
+    for (let vamp of myAncestors) {
+      if (vampireAncestors.includes(vamp)) return vamp;
+    }
   }
 }
 
